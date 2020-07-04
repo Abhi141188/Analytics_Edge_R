4214f239a8736b4de2c6588521efd31cb1c3e0ee
abhinav choudhary <55540924+Abhi141188@users.noreply.github.com>
2020-07-04 12:08:56 +0530

    Prgrm - 1

--- Vectors-DataStructuresinR.R
+++ Vectors-DataStructuresinR.R
@@ -2,6 +2,7 @@
 # A vector is a one-dimensional array. 
 # We can create a vector with all the basic data type
 # The simplest way to build a vector is to use the c command.
+
 # Creating a Numerical Vector:
 vec_num <- c(1, 10, 49)
 vec_num
@@ -22,9 +23,12 @@ class(vec_random)
 # Object Properties
 #vector
 v1= 1:100
-class(v1) ; typeof(v1)
+class(v1) ; 
+typeof(v1)
+
 v2=letters[1:10]
-class(v2) ; typeof(v2)
+class(v2) ; 
+typeof(v2)
 length(v2)
 
 # Arithmetic calculations on vectors:
