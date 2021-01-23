4c4bf6570b094e4402d6d4de1db031ee7ca1c8be
abhinav choudhary <55540924+Abhi141188@users.noreply.github.com>
2021-01-23 14:10:57 +0530

    Prgrm - 17

--- MultipleRegression_Project.R
+++ MultipleRegression_Project.R
@@ -46,18 +46,17 @@ View(insurance_premium1)
 #---------------------Company Division---------------------------
 table(df$Employee_Division)/nrow(df)
 # Avg Premium by location
-sqldf::sqldf("select Employee_Division, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
+sqldf("select Employee_Division, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
 #Create dummy variables for Division
 #Here the no. of levels are 5 but no. of observations are high in only 2 levels.
 #Hence,no.of dummy var will be 2-1 = 1.
 insurance_premium1$Division_1 <- ifelse(insurance_premium1$Employee_Division=='IHUT1',1,0)
-insurance_premium1$Division_2 <- ifelse(insurance_premium1$Employee_Division=='IHDR1',1,0)
 
 View(insurance_premium1)
 #---------------------Age_Grp--------------------------
 table(df$Age_Grp)/nrow(df)
 # Avg Premium by Age_Grp
-sqldf::sqldf("select Age_Grp, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
+sqldf("select Age_Grp, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
 #Create dummy variables for Age_Grp
 #Here the no. of levels are 3 and all 3 have large no. of observations.
 #Hence,no.of dummy var will be 3-1=2.
@@ -68,18 +67,17 @@ View(insurance_premium1)
 #---------------------Gender--------------------------
 table(df$Gender)/nrow(df)
 # Avg Premium by Gender
-sqldf::sqldf("select Gender, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
+sqldf("select Gender, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
 #Create dummy variables for Gender
 #Here the no. of levels are 2 but only 2 have large no. of observations.
 #Hence,no.of dummy var will be 2-1= 1.
 insurance_premium1$Gender1 <- ifelse(insurance_premium1$Gender=='Male',1,0)
-insurance_premium1$Gender2 <- ifelse(insurance_premium1$Gender=='Female',1,0)
 
 View(insurance_premium1)
 #---------------------Annual_Salary_Bucket--------------------------
 table(df$Annual_Salary_Bucket)/nrow(df)
 # Avg Premium by Annual_Salary_Bucket
-sqldf::sqldf("select Annual_Salary_Bucket, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
+sqldf("select Annual_Salary_Bucket, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
 #Create dummy variables for Annual_Salary_Bucket
 #Here the no. of levels are 4 and all 4 have significant no. of observations.
 #Hence,no.of dummy var will be 4-1= 3.
@@ -91,7 +89,7 @@ View(insurance_premium1)
 #---------------------Exp_Within_Org--------------------------
 table(df$Exp_Within_Org)/nrow(df)
 # Avg Premium by Exp_Within_Org
-sqldf::sqldf("select Exp_Within_Org, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
+sqldf("select Exp_Within_Org, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
 #Create dummy variables for Exp_Within_Org
 #Here the no. of levels are 4 but 3 have significant no. of observations.
 #Hence,no.of dummy var will be 3-1= 2.
@@ -112,7 +110,7 @@ View(insurance_premium1)
 #---------------------Coverage_Level--------------------------
 table(df$Coverage_Level)/nrow(df)
 # Avg Premium by Coverage_Level
-sqldf::sqldf("select Coverage_Level, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
+sqldf("select Coverage_Level, count(*) as obs, avg(Annual_EE_Contri)  from df  group by 1")
 
 #Create dummy variables for Coverage.Level
 #Here the no. of levels are 6 but 3 have significant no. of observations.
@@ -122,15 +120,13 @@ insurance_premium1$Coverage.Level2 <- ifelse(insurance_premium1$Coverage_Level==
 
 View(insurance_premium1)
 #-------Regression Model--------------
-reg_ins_formula <- lm(formula=Annual_EE_Contri~ #State3
-                        #+State2
-                        #+State1+
-                        #Marital_status1+#Division_1
-                        +Division_2+
+reg_ins_formula <- lm(Annual_EE_Contri ~ #State3+
+                        #State2+
+                        #State1+
+                        #Marital_status1+#Division_1+
                         Age_Grp1+Age_Grp2+#Gender1+
-                        #Gender2+
-                        #Annual_Salary_Bucket1+#Annual_Salary_Bucket2
-                        #+Annual_Salary_Bucket3+
+                        #Annual_Salary_Bucket1+Annual_Salary_Bucket2+
+                        #Annual_Salary_Bucket3+
                         #Exp_Within_Org1+#Exp_Within_Org2+
                         Employee_Class1+Coverage.Level1+Coverage.Level2,
                       data=insurance_premium1)
@@ -143,19 +139,18 @@ setwd("F:/rWork/rProjects/Analytics_Edge/Analytics_Edge_R")
 sm2 <- read.csv("data/InsurancePlanSubscriptions-2018.csv")
 View(sm2)
 #From the regression eqn/model build above, we can see that variables: 
-#Age_Grp2, Age_Grp1, Division_2, Employee.Class1, Coverage.Level2,Coverage.Level1
+#Age_Grp2, Age_Grp1,Employee.Class1, Coverage.Level2,Coverage.Level1
 #are significant but they dont exist in 2018 dataset, so we add them in 2018 dataset
 #and then run the predict function for predicitve modeling.
 
-#Adding var column: Age_Grp2, Age_Grp1, Divison_2, Employee.Class1, 
-#Coverage.Level2,Coverage.Level1 in the 2018 dataset
+#Adding var column:Age_Grp2, Age_Grp1, Employee.Class1, Coverage.Level2,Coverage.Level1 in the 2018 dataset
 sm2$Coverage.Level1 <- ifelse(sm2$Coverage_Level=='Employee',1,0)
 sm2$Coverage.Level2 <- ifelse(sm2$Coverage_Level=='Employee, Spouse & Children',1,0)
-sm2$Division_2 <- ifelse(sm2$Employee_Division=='IHDR1',1,0)
 sm2$Age_Grp1 <- ifelse(sm2$Age_Grp=='Millennials [15-36]yrs',1,0)
 sm2$Age_Grp2 <- ifelse(sm2$Age_Grp=='Generation X [37-52]yrs',1,0)
 sm2$Employee_Class1 <- ifelse(sm2$Employee_Class=='Full Time-Hourly',1,0)
 
+View(sm2)
 #Predict----
 sm2$Pred_EE_Contri <- predict(reg_ins_formula,newdata=sm2)
 View(sm2)
