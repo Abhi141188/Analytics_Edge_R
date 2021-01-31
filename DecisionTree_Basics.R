a41fe0cd54b872ead9ad70bc2b583b3c3f58fb92
abhinav choudhary <55540924+Abhi141188@users.noreply.github.com>
2021-01-31 13:51:08 +0530

    Prgrm - 20

--- DecisionTree_Basics.R
+++ DecisionTree_Basics.R
@@ -1,5 +1,5 @@
 #Decision Tree- 
-#Regression: Predicitng Continuous Dependent Var Values.
+#Regression: Predicting Continuous Dependent Var Values.
 #Classification: Predicting Categorical Dependent Var Values.
 #Pruning:It is a technique used in determining the size of the tree.
 
@@ -8,6 +8,7 @@
 library("rpart")
 library("rpart.plot")
 #Read dataset----
+?iris
 data("iris")
 data_iris <- iris
 View(data_iris)
@@ -23,9 +24,17 @@ View(iris_train)
 View(iris_test)
 
 #Build and plot model----
-#Classification Tree-
+#Classification Decision Tree-
 ctree = rpart(Species ~., data = iris_train, method = "class")
+ctree
 rpart.plot(ctree)
+
+#Predicting:----
+#Classification Tree: Predicting whether the species is "Setosa" or not,
+iris_test$Prediction_Species = predict(ctree, iris_test, type = 'class')
+View(iris_test)
+
+#Accuracy (Confusion Matrix)
 
 #Regression Tree-
 rtree = rpart(Sepal.Length ~., data = iris_train, method = "anova")
@@ -33,19 +42,13 @@ rpart.plot(rtree)
 print(ctree)
 
 #Predicitng:----
-#Classification Tree: Predicting whether the species is "Setosa" or not,
-iris_test$Prediction_Species = predict(ctree, iris_test)
-View(iris_test)
-
 #Regression Tree: Predicting the lenth of Sepal,
 iris_test$Prediction_Sepal.Length = predict(rtree, iris_test)
 View(iris_test)
 
-#Pruning----
-tree_ms3 = rpart(Species~., iris_train,control = rpart.control(minsplit = 3))
-tree_ms50 = rpart(Species~., iris_train, control = rpart.control(minsplit = 50))
+#Accuracy (MAPE)
 
-par(mfcol = c(1, 2))
-rpart.plot(tree_ms3, main = "minsplit=3")
-rpart.plot(tree_ms50, main = "minsplit=50")
+#Pruning----
+tree_ms5 = rpart(Species~., iris_train,control = rpart.control(minsplit = 5))
+rpart.plot(tree_ms5, main = "minsplit=5")
 
